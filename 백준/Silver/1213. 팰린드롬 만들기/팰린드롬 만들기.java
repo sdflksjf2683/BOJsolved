c97cf8f6b6e2b46cdae695ab97fa310ad29e673b
sdflksjf2683 <78345851+sdflksjf2683@users.noreply.github.com>
2022-08-12 00:56:25 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 160 ms, Memory: 16284 KB -BaekjoonHub

---
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s = br.readLine();
+		String ans = "";
+
+		int[] abc = new int[26];
+		int one = 0;
+		int oneidx = -1;
+
+		for(int i=0;i<s.length();i++)
+			abc[s.charAt(i)-'A']++;
+		
+		for(int i=0;i<abc.length;i++) {
+			if(abc[i]%2!=0) {
+				one++;
+				oneidx = i;
+			}
+		}
+		
+		if(one>1) 
+			System.out.println("I'm Sorry Hansoo");
+		else {
+			for(int i=0;i<26;i++) {
+				for(int j=0;j<abc[i]/2;j++) {
+					ans+=(char)(i+'A');
+				}
+			}
+			if(one==1)
+				ans += (char)(oneidx+'A');
+			for(int i=25;i>=0;i--) {
+				for(int j=0;j<abc[i]/2;j++) {
+					ans+=(char)(i+'A');
+				}
+			}
+			System.out.println(ans);
+		}
+		
+	}
+}
