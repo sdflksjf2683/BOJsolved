535bc0dae281316ff1fcfe4814318b21133c68b7
sdflksjf2683 <78345851+sdflksjf2683@users.noreply.github.com>
2022-11-05 22:37:33 +0900

    [Silver IV] Title: 걷기, Time: 208 ms, Memory: 17684 KB -BaekjoonHub

--- 백준/Silver/1459. 걷기/걷기.java
+++ 백준/Silver/1459. 걷기/걷기.java
@@ -1,5 +1,14 @@
 import java.util.Scanner;
 
+/*
+ * 최단 거리의 경우의 수를 고려하여 거리를 계산한다.
+ * 1. 대각선 한 번 이동하는 것보다 가로+세로 이동하는 것이 더 빠른 경우
+ * 2. 가로+세로 이동보다 대각선 이동이 더 빠른경우
+ * 	2-1. 대각선으로만 이동이 가능한 경우 
+ * 	2-2. 대각선 이동과 가로 또는 세로 이동이 한 번 필요한 경우 
+ * 3. 대각선 이동과 가로+세로 이동을 모두 사용해야 하는 경우
+ * */
+
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -10,19 +19,15 @@ public class Main {
 		
 		long result = 0;
 		
-		//가로세로보다 대각선이 빠른 경우
 		
 		if(2*w < s) {//대각선 한번보다 가로+세로 이동이 더 빠른 경우
 			result = (x+y)*w; //가로+세로로만 이동 
 		}
 		
+		//가로세로보다 대각선이 빠른 경우
 		else if(w > s) {
 			if((x+y)%2==0) result = Math.max(x, y)*s; //대각선으로만 이동 가능
 			else result = (Math.max(x, y)-1)*s + w; //대각선 + 마지막에 가로/세로
-		}
-		
-		else if(x==y) { //대각선으로 일직선 이동
-			result = y*s;
 		}
 		
 		else { 
