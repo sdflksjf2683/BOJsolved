f8fac066da00aa0f1135f876208b29fb6bcc3167
백자민(Baik Jamin) <78345851+sdflksjf2683@users.noreply.github.com>
2023-06-27 11:24:17 +0900

    [level 3] Title: 양과 늑대, Time: 8.54 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/92343. 양과 늑대/양과 늑대.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Solution {
+    
+    static int[] info;
+    static int[][] edges;
+    static int max;
+    
+    static void dfs(int sheep, int wolf, int temp, boolean[] visit) {
+        visit[temp] = true;
+        if(info[temp]==1) { //늑대가 있는 노드
+            if(wolf+1>=sheep) //늑대가 한 마리 더 늘어나서 양을 잡아먹을 경우 종료
+                return;
+            wolf++;
+        } else { //양이 있는 노드
+            sheep++;
+            if(sheep>max)
+                max = sheep;
+        }
+        
+        for(int[] edge: edges) {
+            if(visit[edge[0]] && !visit[edge[1]]) {
+                boolean[] nvisit = visit.clone();
+                dfs(sheep, wolf, edge[1], nvisit);
+            }
+                
+        }
+        
+    }
+    
+    public int solution(int[] tinfo, int[][] tedges) {
+        max = 0;
+        info = tinfo;
+        edges = tedges;
+        boolean[] visit = new boolean[info.length];
+        dfs(0,0,0,visit);
+        
+        return max;
+    }
+}
