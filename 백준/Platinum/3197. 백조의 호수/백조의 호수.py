07df4f68e566073128630946173e2e381f910c5f
백자민(Baik Jamin) <78345851+sdflksjf2683@users.noreply.github.com>
2024-09-04 18:25:43 +0900

    [Platinum V] Title: 백조의 호수, Time: 3452 ms, Memory: 209156 KB -BaekjoonHub

--- 백준/Platinum/3197. 백조의 호수/백조의 호수.py
+++ 백준/Platinum/3197. 백조의 호수/백조의 호수.py
@@ -3,15 +3,15 @@ from collections import deque
 
 input = sys.stdin.readline
 
-delta = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+d = [(-1, 0), (0, 1), (1, 0), (0, -1)]
 
 def solution():
-
+    #Union-find 용 함수들
     def find(n):
         temp = n
-        while group[n] != n:
-            n = group[n]
-        group[temp] = n
+        while parent[n] != n:
+            n = parent[n]
+        parent[temp] = n
         return n
 
     def union(n1, n2):
@@ -19,51 +19,66 @@ def solution():
         n2 = find(n2)
         if n1 > n2:
             n1, n2 = n2, n1
-        group[n2] = n1
+        parent[n2] = n1
 
-    R, C = map(int, input().split())
-    board = [[*input().rstrip()] for _ in range(R)]
-    visited = [[0]*C for _ in range(R)]
-    group = [i for i in range(R*C)]
-    swans = []
-    _queue = deque()
+    R,C = map(int, input().split())
+    board = [[*input().rstrip()] for _ in range(R)] 
+    parent = [i for i in range(R*C)] #각 물의 그룹을 저장할 배열
+    visit = [[0] * C for _ in range(R)]
+    swan = []
+
+    _queue = deque() #물 위치를 저장할 큐
     for i in range(R):
         for j in range(C):
-            if visited[i][j] == 0 and board[i][j] != 'X':
-                queue = deque([(i, j)])
-                visited[i][j] = 1
-                while queue:
-                    r, c = queue.popleft()
-                    if board[r][c] == 'L':
-                        swans.append((r, c))
-                    for dr, dc in delta:
-                        nr, nc = r+dr, c+dc
-                        if R > nr >= 0 and C > nc >= 0:
-                            if visited[nr][nc] == 0:
-                                if board[nr][nc] != 'X':
-                                    queue.append((nr, nc))
-                                    union(r*C+c, nr*C+nc)
+            #이미 방문했거나 빙하인 경우 패스
+            if visit[i][j]==0 and board[i][j]!='X':
+                q = deque([(i,j)])
+                visit[i][j] = 1
+
+                while q:
+                    ti,tj = q.popleft()
+                    if board[ti][tj] == 'L':
+                        swan.append((ti,tj))
+                    
+                    for di,dj in d:
+                        ni = ti+di
+                        nj = tj+dj
+
+                        if 0<=ni<R and 0<=nj<C:
+                            if visit[ni][nj]==0:
+                                if board[ni][nj]!='X':
+                                    q.append((ni,nj))
+                                    union(ti*C+tj, ni*C+nj)
                                 else:
-                                    _queue.append((nr, nc))
-                                visited[nr][nc] = 1
-    idx1 = swans[0][0]*C+swans[0][1]
-    idx2 = swans[1][0]*C+swans[1][1]
-    queue = _queue
-    day = 0
-    while find(idx1) != find(idx2):
-        day += 1
+                                    _queue.append((ni,nj))
+                                
+                                visit[ni][nj] = 1
+
+    #두 백조의 그룹
+    swan1 = swan[0][0]*C + swan[0][1]
+    swan2 = swan[1][0]*C + swan[1][1]
+    ans = 0
+    q = _queue
+    #두 백조의 그룹이 같아지면 만날 수 있는 것
+    while find(swan1) != find(swan2):
+        ans +=1
         _queue = deque()
-        while queue:
-            r, c = queue.popleft()
-            for dr, dc in delta:
-                nr, nc = r+dr, c+dc
-                if R > nr >= 0 and C > nc >= 0:
-                    if visited[nr][nc] == 0:
-                        visited[nr][nc] = day+1
-                        _queue.append((nr, nc))
-                    elif visited[nr][nc] != day+1:
-                        union(r*C+c, nr*C+nc)
-        queue = _queue
-    print(day)
+        while q:
+            ti,tj = q.popleft()
+            for di,dj in d:
+                ni = ti+di
+                nj = tj+dj
+
+                if 0<=ni<R and 0<=nj<C:
+                    if visit[ni][nj]==0:
+                        visit[ni][nj] = ans+1
+                        _queue.append((ni,nj))
+                    elif visit[ni][nj] != ans+1:
+                        union(ti*C+tj, ni*C+nj)
+        
+        #일괄변경
+        q = _queue
+
+    print(ans)
 
 solution()
