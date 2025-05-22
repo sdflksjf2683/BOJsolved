e3be2e7611720d3092c54cac0f6412521cc9b784
백자민(Baik Jamin) <78345851+sdflksjf2683@users.noreply.github.com>
2025-05-22 21:49:25 +0900

    [Gold IV] Title: 우체국, Time: 188 ms, Memory: 48072 KB -BaekjoonHub

--- 백준/Gold/2285. 우체국/우체국.py
+++ 백준/Gold/2285. 우체국/우체국.py
@@ -1,12 +1,27 @@
-<html>
-<head><title>504 Gateway Time-out</title></head>
-<body>
-<center><h1>504 Gateway Time-out</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+import sys
+
+input = lambda:sys.stdin.readline().rstrip()
+
+def main():
+    N = int(input())
+    p_sum = 0 #전체 사람수 저장할 변수
+    village = []
+        
+    for _ in range(N):
+        x,a = map(int, input().split())
+        p_sum += a
+        village.append((x,a))
+    #end input
+    
+    village.sort(key=lambda x:x[0]) #마을 위치 기준 오름차순 정렬
+    
+    cnt=0
+    for x,a in village:
+        cnt+=a
+        if cnt>=(p_sum/2):
+            print(x)
+            break
+        
+    
+if __name__=="__main__":
+    main()
